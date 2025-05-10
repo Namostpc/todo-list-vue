6c176b1e4525a8b823b75d843153bd2ef02cb91e
Namostpc <namorotee@gmail.com>
2025-05-10 18:41:58 +0700

    edit Editview loading

--- src/views/EditView.vue
+++ src/views/EditView.vue
@@ -3,17 +3,21 @@ import { useTodoStore } from "@/stores/todo";
 import { useRoute, RouterLink } from "vue-router";
 import { ref, onMounted } from "vue";
 import "../global.css";
+import Loading from "@/components/loading.vue";
 
 const todoStore = useTodoStore();
 const todoId = ref(-1);
 const route = useRoute();
 const isLoaded = ref(false);
+const isDeleted = ref(false);
+const isUpdated = ref(false);
 
 onMounted(async () => {
+    isLoaded.value = true;
   await todoStore.loadTodo(route.params.id);
   todoId.value = parseInt(route.params.id);
-  isLoaded.value = true;
   console.log(todoStore.selectedTodo);
+  isLoaded.value = false;
 });
 
 const editTodo = async (selectedTodo) => {
@@ -22,8 +26,10 @@ const editTodo = async (selectedTodo) => {
       name: todoStore.selectedTodo.name,
       status: todoStore.selectedTodo.status,
     };
+    isLoaded.value = true;
     await todoStore.editTodo(bodyData, todoId.value);
-    alert("edit Competed");
+    isUpdated.value = true;
+    isLoaded.value = false;
   } catch (error) {
     console.log("error", error);
   }
@@ -34,8 +40,8 @@ const deleteTodo = async (todoId) => {
     isLoaded.value = true;
     await todoStore.removeTodo(todoId);
     await todoStore.loadTodos();
+    isDeleted.value = true;
     isLoaded.value = false;
-    alert("Delete todo list completed");
   } catch (error) {
     console.log("error", error);
   }
@@ -43,66 +49,112 @@ const deleteTodo = async (todoId) => {
 </script>
 
 <template>
-  <div>
-    <RouterLink :to="{ name: 'home' }">
-      <button
-        class="mx-3 min-w-16 text-white font-bold py-2 px-4 rounded-full cursor-pointer"
-      >
-        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 576 512">
-          <path
-            d="M575.8 255.5c0 18-15 32.1-32 32.1l-32 0 .7 160.2c0 2.7-.2 5.4-.5 8.1l0 16.2c0 22.1-17.9 40-40 40l-16 0c-1.1 0-2.2 0-3.3-.1c-1.4 .1-2.8 .1-4.2 .1L416 512l-24 0c-22.1 0-40-17.9-40-40l0-24 0-64c0-17.7-14.3-32-32-32l-64 0c-17.7 0-32 14.3-32 32l0 64 0 24c0 22.1-17.9 40-40 40l-24 0-31.9 0c-1.5 0-3-.1-4.5-.2c-1.2 .1-2.4 .2-3.6 .2l-16 0c-22.1 0-40-17.9-40-40l0-112c0-.9 0-1.9 .1-2.8l0-69.7-32 0c-18 0-32-14-32-32.1c0-9 3-17 10-24L266.4 8c7-7 15-8 22-8s15 2 21 7L564.8 231.5c8 7 12 15 11 24z"
-          />
-        </svg>
-      </button>
-    </RouterLink>
-    <div
-      v-if="isLoaded"
-      class="flex flex-col w-full items-center justify-center w-full mx-auto"
+  <Loading v-if="isLoaded"></Loading>
+  <RouterLink :to="{ name: 'home' }">
+    <button
+      class="mx-3 min-w-16 text-white font-bold py-2 px-4 rounded-full cursor-pointer"
     >
-      <div class="mb-6">
-        <label
-          for="text-box"
-          class="block text-sm font-medium text-gray-900 dark:text-white w-100"
-          >Todo Name</label
-        >
-        <input
-          type="text"
-          id="text-box"
-          v-model="todoStore.selectedTodo.name"
-          class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 576 512">
+        <path
+          d="M575.8 255.5c0 18-15 32.1-32 32.1l-32 0 .7 160.2c0 2.7-.2 5.4-.5 8.1l0 16.2c0 22.1-17.9 40-40 40l-16 0c-1.1 0-2.2 0-3.3-.1c-1.4 .1-2.8 .1-4.2 .1L416 512l-24 0c-22.1 0-40-17.9-40-40l0-24 0-64c0-17.7-14.3-32-32-32l-64 0c-17.7 0-32 14.3-32 32l0 64 0 24c0 22.1-17.9 40-40 40l-24 0-31.9 0c-1.5 0-3-.1-4.5-.2c-1.2 .1-2.4 .2-3.6 .2l-16 0c-22.1 0-40-17.9-40-40l0-112c0-.9 0-1.9 .1-2.8l0-69.7-32 0c-18 0-32-14-32-32.1c0-9 3-17 10-24L266.4 8c7-7 15-8 22-8s15 2 21 7L564.8 231.5c8 7 12 15 11 24z"
         />
-      </div>
-      <div class="w-100 my-1">
-        <label
-          for="select-status"
-          class="block text-sm font-medium text-gray-900 dark:text-white"
-          >Select an option</label
+      </svg>
+    </button>
+  </RouterLink>
+  <div class="flex flex-col w-full items-center mx-auto my-20">
+    <div class="mb-6">
+      <label
+        for="text-box"
+        class="block text-sm font-medium text-gray-900 dark:text-white w-100"
+        >Todo Name</label
+      >
+      <input
+        type="text"
+        id="text-box"
+        v-model="todoStore.selectedTodo.name"
+        class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-100 p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+      />
+    </div>
+    <div class="w-100 my-1">
+      <label
+        for="select-status"
+        class="block text-sm font-medium text-gray-900 dark:text-white"
+        >Select an option</label
+      >
+      <select
+        id="select-status"
+        v-model="todoStore.selectedTodo.status"
+        class="w-full bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+      >
+        <option>Select Status</option>
+        <!-- ต้องทำการ blinding value เข้าไปด้วยเพื่อให้ option มีค่าส่งกลับไปหาฟอร์มได้-->
+        <option v-for="status in todoStore.statuses" :value="status">
+          {{ status }}
+        </option>
+      </select>
+      <div>
+        <div
+          v-if="isDeleted"
+          id="toast-danger"
+          class="flex items-center w-100 p-4 mt-4 text-gray-500 bg-white rounded-lg shadow-sm dark:text-gray-400 dark:bg-gray-800"
+          role="alert"
         >
-        <select
-          id="select-status"
-          v-model="todoStore.selectedTodo.status"
-          class="w-full bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+          <div
+            class="inline-flex items-center justify-center shrink-0 w-10 h-8 text-red-500 bg-red-100 rounded-lg dark:bg-red-800 dark:text-red-200"
+          >
+            <svg
+              class="w-5 h-5"
+              aria-hidden="true"
+              xmlns="http://www.w3.org/2000/svg"
+              fill="currentColor"
+              viewBox="0 0 20 20"
+            >
+              <path
+                d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5Zm3.707 11.793a1 1 0 1 1-1.414 1.414L10 11.414l-2.293 2.293a1 1 0 0 1-1.414-1.414L8.586 10 6.293 7.707a1 1 0 0 1 1.414-1.414L10 8.586l2.293-2.293a1 1 0 0 1 1.414 1.414L11.414 10l2.293 2.293Z"
+              />
+            </svg>
+            <span class="sr-only">Error icon</span>
+          </div>
+          <div class="ms-3 text-sm font-normal">Item has been deleted.</div>
+        </div>
+        <div
+          v-if="isUpdated"
+          id="toast-success"
+          class="flex items-center w-100 p-4 mt-4 text-gray-500 bg-white rounded-lg shadow-sm dark:text-gray-400 dark:bg-gray-800"
+          role="alert"
         >
-          <option>Select Status</option>
-          <!-- ต้องทำการ blinding value เข้าไปด้วยเพื่อให้ option มีค่าส่งกลับไปหาฟอร์มได้-->
-          <option v-for="status in todoStore.statuses" :value="status">
-            {{ status }}
-          </option>
-        </select>
-        <div>
+          <div
+            class="inline-flex items-center justify-center shrink-0 w-8 h-8 text-green-500 bg-green-100 rounded-lg dark:bg-green-800 dark:text-green-200"
+          >
+            <svg
+              class="w-5 h-5"
+              aria-hidden="true"
+              xmlns="http://www.w3.org/2000/svg"
+              fill="currentColor"
+              viewBox="0 0 20 20"
+            >
+              <path
+                d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5Zm3.707 8.207-4 4a1 1 0 0 1-1.414 0l-2-2a1 1 0 0 1 1.414-1.414L9 10.586l3.293-3.293a1 1 0 0 1 1.414 1.414Z"
+              />
+            </svg>
+            <span class="sr-only">Check icon</span>
+          </div>
+
+          <div class="ms-3 text-sm font-normal">Update successfully.</div>
+        </div>
+        
           <button
             class="mx-auto my-5 min-w-20 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full cursor-pointer"
             @click="editTodo(todoStore.selectedTodo.id)"
           >
             Edit
           </button>
-          <button
-            @click="deleteTodo(todoStore.selectedTodo.id)"
-            class="p-1 my-3 mx-3 min-w-16 bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded-full items-center cursor-pointer"
-          >
-            Delete
-          </button>
-        </div>
+        <button
+          @click="deleteTodo(todoStore.selectedTodo.id)"
+          class="p-1 my-3 mx-3 min-w-16 bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded-full items-center cursor-pointer"
+        >
+          Delete
+        </button>
       </div>
     </div>
   </div>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,6 +2,7 @@
 import { ref, computed, onMounted } from "vue";
 import { useTodoStore } from "@/stores/todo";
 import { RouterLink } from "vue-router";
+import loading from "@/components/loading.vue";
 
 import "../global.css";
 
@@ -9,11 +10,11 @@ const todoStore = useTodoStore();
 const todoText = ref("");
 const isLoading = ref(false);
 
-const selectedStatus = ref('Pending')
+const selectedStatus = ref("Pending");
 
 const filterTodoList = computed(() => {
-  return todoStore.list.filter(todo => todo.status === selectedStatus.value)
-})
+  return todoStore.list.filter((todo) => todo.status === selectedStatus.value);
+});
 
 onMounted(async () => {
   isLoading.value = true;
@@ -69,24 +70,24 @@ const changeStatus = async (event, todoId) => {
     console.log("error", error);
   }
   await todoStore.loadTodos();
-
 };
 
 const changeSelectedStatus = async (newStatus) => {
-  selectedStatus.value = newStatus
-}
+  selectedStatus.value = newStatus;
+};
 
 const changeDoingStatus = async (todoId) => {
-    try {
-      await todoStore.editTodo({ status: "Doing" }, todoId);
-    }catch (error) {
-      console.log('error', error);
-    }
-    await todoStore.loadTodos();
-}
+  try {
+    await todoStore.editTodo({ status: "Doing" }, todoId);
+  } catch (error) {
+    console.log("error", error);
+  }
+  await todoStore.loadTodos();
+};
 </script>
 
 <template>
+  <loading v-if="isLoading"></loading>
   <div
     class="flex flex-col justify-center my-8 mx-8 outline-rose-500 h-screen w-full"
   >
@@ -95,56 +96,37 @@ const changeDoingStatus = async (todoId) => {
     >
       To do List
     </div>
-    <div
-      v-if="isLoading"
-      class="flex justify-center items-center text-center fixed inset-0 bg-black bg-opacity-30"
-    >
-      <div role="status">
-        <svg
-          aria-hidden="true"
-          class="inline w-50 h-50 text-gray-200 animate-spin dark:text-gray-600 fill-blue-600"
-          viewBox="0 0 100 101"
-          fill="none"
-          xmlns="http://www.w3.org/2000/svg"
-        >
-          <path
-            d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z"
-            fill="currentColor"
-          />
-          <path
-            d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z"
-            fill="currentFill"
-          />
-        </svg>
-        <span class="sr-only">Loading...</span>
-      </div>
-    </div>
     <div class="flex flex-row justify-center w-full">
-      <div v-if="!isLoading" class="relative">
         <input
           type="text"
           v-model="todoText"
           id="text-add"
-          class="block w-180 p-4 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+          class="block w-150 p-4 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
           placeholder="Add To do list"
         />
         <button
           @click="addtodo(todoText)"
-          class="text-white absolute end-2.5 bottom-2.5 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+          class="text-white ml-1 w-50 end-2.5 bottom-2.5 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
         >
           Add
         </button>
-      </div>
     </div>
     <div>
       <div
-        class="flex flex-wrap items-center justify-center mt-10  text-m font-medium text-center text-gray-500  border-b border-gray-200 dark:border-gray-700 dark:text-gray-400"
+        class="flex flex-wrap items-center justify-center mt-10 text-m font-medium text-center text-gray-500 border-b border-gray-200 dark:border-gray-700 dark:text-gray-400"
       >
-        <div v-for="status in todoStore.statuses" class="w-36 ml-1 items-center p-4 text-blue-600 bg-gray-700 hover:bg-gray-700 rounded-t-lg cursor-pointer cursor-pointer"  
-        :class="status === selectedStatus ? 'active dark:bg-purple-900 dark:text-white': ''"
-        @click="changeSelectedStatus(status)" >
+        <div
+          v-for="status in todoStore.statuses"
+          class="w-36 ml-1 items-center p-4 text-blue-600 bg-gray-700 hover:bg-gray-700 rounded-t-lg cursor-pointer cursor-pointer"
+          :class="
+            status === selectedStatus
+              ? 'active dark:bg-purple-900 dark:text-white'
+              : ''
+          "
+          @click="changeSelectedStatus(status)"
+        >
           <a>
-            {{status}}
+            {{ status }}
           </a>
         </div>
       </div>
@@ -171,10 +153,15 @@ const changeDoingStatus = async (todoId) => {
           {{ todo.name }}
         </div>
         <div>
-          <button @click="changeDoingStatus(todo.id)" class="mx-3 min-w-16 bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded-full cursor-pointer"
+          <button
+            @click="changeDoingStatus(todo.id)"
+            class="mx-3 min-w-16 bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded-full cursor-pointer"
           >
             <svg xmlns="http://www.w3.org/2000/svg" viewBox="-250 -180 900 900">
-              <path d="M0 64C0 28.7 28.7 0 64 0L224 0l0 128c0 17.7 14.3 32 32 32l128 0 0 288c0 35.3-28.7 64-64 64L64 512c-35.3 0-64-28.7-64-64L0 64zm384 64l-128 0L256 0 384 128z"/></svg>
+              <path
+                d="M0 64C0 28.7 28.7 0 64 0L224 0l0 128c0 17.7 14.3 32 32 32l128 0 0 288c0 35.3-28.7 64-64 64L64 512c-35.3 0-64-28.7-64-64L0 64zm384 64l-128 0L256 0 384 128z"
+              />
+            </svg>
           </button>
         </div>
         <RouterLink :to="{ name: 'todo-edit', params: { id: todo.id } }">
@@ -194,7 +181,7 @@ const changeDoingStatus = async (todoId) => {
         </RouterLink>
         <button
           @click="deleteTodo(todo.id)"
-          class=" p-1 my-3 mx-3 min-w-16 bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded-full items-center cursor-pointer"
+          class="p-1 my-3 mx-3 min-w-16 bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded-full items-center cursor-pointer"
         >
           <svg xmlns="http://www.w3.org/2000/svg" viewBox="-250 -190 900 900">
             <path
